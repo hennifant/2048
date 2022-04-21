30e84e2fac6652692190ca407d3cbba75acc7c0f
hennifant <henning.schwichow@gmx.de>
2022-04-21 18:58:33 +0200

    Added rotate function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import { useSwipeable } from 'react-swipeable';
 
 import TileGrid from './components/TileGrid.js';
 
+const rotateLeft = (tiles, size) => {
+  const newTiles = [];
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      let k = size - 1 - i;
+      newTiles[j + i * size] = tiles[k + j * size];
+    }
+  }
+  return newTiles;
+};
+
+const rotateRight = (tiles, size) => {
+  const newTiles = [];
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      let k = size - 1 - j;
+      newTiles[j + i * size] = tiles[i + k * size];
+    }
+  }
+  return newTiles;
+};
+
 function App() {
   return (
     <div className="App">
