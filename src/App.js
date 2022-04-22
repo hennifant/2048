f22cd0ae9336717ee8a2a1de6c94df7816f7b1f1
hennifant <henning.schwichow@gmx.de>
2022-04-22 14:21:12 +0200

    Added compress function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useSwipeable } from 'react-swipeable';
 import styled from 'styled-components';
+
+import compress from './components/Compress.js';
 import addRandomTile from './components/RandomTile.js';
 import checkGameOver from './components/GameOver.js';
 
@@ -32,6 +34,58 @@ function App() {
   const [size, setSize] = useState(4);
   const [tiles, setTiles] = useState(() => {
     return addRandomTile(addRandomTile(new Array(size * size).fill(0)));
+  });
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  const handleSwipe = event => {
+    if (checkGameOver(tiles, size)) {
+      setIsGameOver(true);
+    } else {
+      let T = [];
+      switch (event.dir) {
+        case 'Up':
+          //left//compress//right
+          T = rotateLeft(tiles, size);
+          T = compress(T, setScore, size);
+          T = rotateRight(T, size);
+          T = addRandomTile(T);
+          setTiles(T);
+          break;
+        case 'Down':
+          //right//compress//left
+          T = rotateRight(tiles, size);
+          T = compress(T, setScore, size);
+          T = rotateLeft(T, size);
+          T = addRandomTile(T);
+          setTiles(T);
+          break;
+        case 'Right':
+          //right //right//compress//left //left
+          T = rotateRight(tiles, size);
+          T = rotateRight(T, size);
+          T = compress(T, setScore, size);
+          T = rotateLeft(T, size);
+          T = rotateLeft(T, size);
+          T = addRandomTile(T);
+          setTiles(T);
+          break;
+        case 'Left':
+          //compress
+          T = compress(tiles, setScore, size);
+          T = addRandomTile(T);
+          setTiles(T);
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  const handlers = useSwipeable({
+    onSwiped: handleSwipe,
+    preventDefaultTouchmoveEvent: true,
   });
   return (
     <AppContainer>
